406f6ffef139ab12bf362939e570782ae0ff73d9
ParthJohri <parthjohri31@gmail.com>
2022-01-04 03:43:37 +0530

    ADDED A SPLASH SCREEN FOR 5 SECONDS

--- app/src/main/java/com/examples/medicinetracker/MainActivity.kt
+++ app/src/main/java/com/examples/medicinetracker/MainActivity.kt
@@ -1,23 +1,30 @@
 package com.examples.medicinetracker
-import android.graphics.Color
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity
-import android.view.View
 
+import android.os.Bundle
 import android.widget.ProgressBar
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.LifecycleCoroutineScope
 import androidx.lifecycle.lifecycleScope
 import androidx.viewpager2.widget.ViewPager2
 import kotlinx.coroutines.delay
 import java.util.*
 
-var counter = 0;
+var counter = 0
+
 class MainActivity : AppCompatActivity() {
     private lateinit var viewPager:ViewPager2
+    private val viewModel:MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply{
+            viewModel.isLoading.value
+        }
         setContentView(R.layout.activity_main)
+//      Removing Action Bar
         supportActionBar?.hide()
+//      Image Slider
         viewPager=findViewById(R.id.viewPager2)
         val imageList=listOf(R.drawable.quote1,R.drawable.quote2,R.drawable.quote3,R.drawable.quote4,R.drawable.quote5 )
         val adapter=ViewPagerAdapter(imageList)
