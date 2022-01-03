41903aa1f5e3b181e9c097b0ae1ad64058156d82
ParthJohri <parthjohri31@gmail.com>
2022-01-03 06:50:07 +0530

    Added a new feature for Ordering Medicines.

--- app/src/main/java/com/examples/medicinetracker/MainActivity.kt
+++ app/src/main/java/com/examples/medicinetracker/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.examples.medicinetracker
-
+import android.graphics.Color
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.view.View
 
+import android.widget.ProgressBar
+import java.util.*
+
+var counter = 0;
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        prog()
     }
+        fun prog(){
+            val pb:ProgressBar = findViewById(R.id.pb)
+            val t = Timer()
+            val tt: TimerTask = object : TimerTask() {
+                override fun run() {
+                    counter++
+                    pb.progress = counter
+                    if (counter === 70) t.cancel()
+                }
+            }
+            t.schedule(tt, 0, 50) }
+
 }
